5d2b3aca1ee20ffa4960d22f65b11eeeb173ebf3
Sean McLeaish <smcleaish@gmail.com>
2024-09-11 05:50:03 -1000

    shadcn installed

--- cml-keycloakify/vite.config.ts
+++ cml-keycloakify/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
       accountThemeImplementation: "none",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
